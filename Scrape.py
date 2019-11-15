0040ae5281b845a621254c424e6ca9566ebe1f7f
Keilon Robertson <43102923+KeilonRobertson@users.noreply.github.com>
2019-11-15 12:58:44 -0400

    Version 1.1
    Basic Lyrics Scraper using Beautiful Soup and Requests

---
+++ Scrape.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+features="html.parser"
+
+class Lyrics():
+    lyrics = ""
+
+    def __init__(self, details):
+        self.details = details
+
+    def getLyrics(self):
+            for x in search(details, tld="co.in", lang='en', start=0, stop=1):
+                    site = x
+            req = requests.get(site)
+            soup = BeautifulSoup(req.content, features)
+            lyrics = ""
+            for link in soup.find_all("p"):
+                lyrics += link.text
+            print(lyrics)
+
+print("1. Get a song")
+print("2. Exit")
+
+print("Enter an option: ")
+option = int(input())
+
+if option == 1:
+        print("Please enter the name of the song, and the artist, followed by enter: ")
+        details = " genius lyrics" + input()
+        scrape = Lyrics(details)
+        print("Lyrics: ")
+        print("")
+        scrape.getLyrics()
+
+elif option == 2:
+        print("Later")
+        exit()
+
+
+
+
+
